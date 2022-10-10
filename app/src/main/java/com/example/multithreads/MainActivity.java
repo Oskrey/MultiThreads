34e9d7f41c9433ec612bb57e5d9ab9fcd1115090
Oskrey <pedichn@gmail.com>
2022-10-10 11:06:28 +0300

    Initial commit

---
+++ app/src/main/java/com/example/multithreads/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.multithreads;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    findViewById(R.id.buttonSync).setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent switcher = new Intent(MainActivity.this, MainActivitySync.class);
+            startActivity(switcher);
+        }
+    });
+
+        findViewById(R.id.buttonAsync).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent switcher = new Intent(MainActivity.this, MainActivityAsync.class);
+                startActivity(switcher);
+            }
+        });
+
+        findViewById(R.id.buttonSync).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
+
+}
